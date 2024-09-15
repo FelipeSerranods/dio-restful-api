81894a5c48c7fdb26b485dae7d875dd7db7df277
FelipeSerrano <felipe.serranods@hotmail.com>
2024-09-14 23:16:28 -0300

    iniciando projeto restful api

---
+++ src/main/java/com/dio/desafio/api/nuvem/lab_banco/LabBancoApplication.java
@@ -0,0 +1,13 @@
+package com.dio.desafio.api.nuvem.lab_banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabBancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/desafio/api/nuvem/lab_banco/LabBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.desafio.api.nuvem.lab_banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
